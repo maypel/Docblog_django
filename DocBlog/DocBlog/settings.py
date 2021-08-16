5c156a9b2e6700086300065717485d7fb0c93d39
Mayel <pelle.mayel@gmail.com>
2021-08-16 10:18:22 +0200

    Initial commit

--- DocBlog/DocBlog/settings.py
+++ DocBlog/DocBlog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#hi4=lcp&n-@u5h-wzfpj_3^-v+s#+!fa5pw4zm82*1pfdo^r9'
+SECRET_KEY = '1234'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
